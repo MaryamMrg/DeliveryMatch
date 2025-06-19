11cc263257501fac733cee6af0b70b0e0a76e187
maryam mrg <maryamelouadaa@gmail.com>
2025-06-19 12:31:16 +0100

    implimenting the getusers() and deleteuser() functions for ADMIN role and updateuser() method

--- Backend/src/main/java/com/example/backend/Dto/UserDto.java
+++ Backend/src/main/java/com/example/backend/Dto/UserDto.java
@@ -10,6 +10,16 @@ public class UserDto {
     private String name;
     private String email;
     private Role role;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getEmail() {
         return email;
     }
--- Backend/src/main/java/com/example/backend/Mapper/UserMapper.java
+++ Backend/src/main/java/com/example/backend/Mapper/UserMapper.java
@@ -2,9 +2,10 @@ package com.example.backend.Mapper;
 
 import com.example.backend.Dto.UserDto;
 import com.example.backend.Model.User;
+import org.mapstruct.Mapper;
 
 import java.util.List;
-
+@Mapper(componentModel = "spring")
 public interface UserMapper {
     User toEntity(UserDto dto);
     UserDto toDto(User user);
