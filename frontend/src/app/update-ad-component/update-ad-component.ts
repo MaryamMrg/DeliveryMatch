c58eae239a9d6aa1d9f7550dbe11d6edbc59b649
maryam mrg <maryamelouadaa@gmail.com>
2025-06-22 16:03:25 +0100

    generating updateAdComponent

---
+++ frontend/src/app/update-ad-component/update-ad-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-ad-component',
+  imports: [],
+  templateUrl: './update-ad-component.html',
+  styleUrl: './update-ad-component.css'
+})
+export class UpdateAdComponent {
+
+}
